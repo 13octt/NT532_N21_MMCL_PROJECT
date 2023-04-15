9d0ea486d58f67099d8f874427f970565fb97087
13octt <88072761+13octt@users.noreply.github.com>
2023-04-15 18:55:35 +0700

    Khai báo

--- app/src/main/java/com/application/iot/MainActivity.java
+++ app/src/main/java/com/application/iot/MainActivity.java
@@ -7,75 +7,32 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-public class MainActivity extends AppCompatActivity implements MqttCallbackExtended{
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final String BROKER_URL = "tcp://34.171.157.201:1883";
-    private static final String CLIENT_ID = "my-android-app";
-    private static final String TOPIC = "dht11/temperature";
-
-    private TextView temp;
-    private TextView humidity;
-    private ProgressBar progressBar;
-    private MqttClientWrapper mqttClientWrapper;
+    private static final String MQTT_BROKER_URI = "tcp://mqtt.eclipse.org:1883";
+    private static final String MQTT_TOPIC_GAS = "sensors/gas";
+    private static final String MQTT_TOPIC_RAIN = "sensors/rain";
+    private static final String MQTT_TOPIC_LIGHT = "sensors/light";
+    private static final String MQTT_TOPIC_TEMP = "sensors/temp";
+    private static final String MQTT_TOPIC_HUMIDITY = "sensors/humidity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        temp = findViewById(R.id.temperature_value_text_view);
-        humidity = findViewById(R.id.humidity_value_text_view);
-        progressBar = findViewById(R.id.progress_bar);
 
-        try {
-            mqttClientWrapper = new MqttClientWrapper(BROKER_URL, CLIENT_ID, TOPIC, this);
-        } catch (MqttException e) {
-            Log.e(TAG, "Error creating MQTT client", e);
-        }
 
-    }
-
-    public void updateUI(final String data){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                String[] values = data.split(",");
-                if (values.length == 2) {
-                    temp.setText(values[0] + "°C");
-                    humidity.setText(values[1] + "%");
-                    progressBar.setVisibility(View.GONE);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void connectComplete(boolean reconnect, String serverURI) {
-        Log.e(TAG, "MQTT connect");
-
-    }
-
-    @Override
-    public void connectionLost(Throwable cause) {
-        Log.e(TAG, "MQTT connection lost", cause);
-    }
-
-    @Override
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
-        String payload = new String(message.getPayload());
-        Log.d(TAG, "Received message on topic " + topic + ": " + payload);
-
-        // TODO: Parse the message payload and update the UI
-        updateUI(payload);
-    }
-
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken token) {
 
     }
 }
