3e67c356c85e584c17b1744460274d86e6540406
13octt <88072761+13octt@users.noreply.github.com>
2023-04-18 05:14:49 +0700

    connect to broker

--- app/src/main/java/com/application/iot/MainActivity.java
+++ app/src/main/java/com/application/iot/MainActivity.java
@@ -1,36 +1,63 @@
 package com.application.iot;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-    private static final String MQTT_BROKER_URI = "tcp://mqtt.eclipse.org:1883";
-    private static final String MQTT_TOPIC_GAS = "sensors/gas";
-    private static final String MQTT_TOPIC_RAIN = "sensors/rain";
-    private static final String MQTT_TOPIC_LIGHT = "sensors/light";
-    private static final String MQTT_TOPIC_TEMP = "sensors/temp";
-    private static final String MQTT_TOPIC_HUMIDITY = "sensors/humidity";
+    String topic = "your/topic";
+    String payload = "{\"temperature\":25.0,\"humidity\":50.0}";
+    private MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String serverUri = "tcp://35.222.45.221:1883";
+        String clientId = "client";
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+
+        String username = "thanhduy";
+        String password = "thanhduy";
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setUserName(username);
+        mqttConnectOptions.setPassword(password.toCharArray());
+
+        try {
+            IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // Connection success
+                    Log.d("MQTT", "Success");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Connection failed
+                    Log.d("MQTT", "Failed");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            mqttAndroidClient.publish(topic, new MqttMessage(payload.getBytes()));
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+
+
+
 
 
 
