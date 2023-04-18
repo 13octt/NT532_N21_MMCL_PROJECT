fed4003b54bba8a2adafa7e8399fa0fd8be66215
13octt <88072761+13octt@users.noreply.github.com>
2023-04-18 10:31:22 +0700

    receive data from broker

--- app/src/main/java/com/application/iot/MainActivity.java
+++ app/src/main/java/com/application/iot/MainActivity.java
@@ -1,19 +1,30 @@
 package com.application.iot;
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
-
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 
 public class MainActivity extends AppCompatActivity {
-    String topic = "your/topic";
-    String payload = "{\"temperature\":25.0,\"humidity\":50.0}";
     private MqttAndroidClient mqttAndroidClient;
+    private static final String TOPIC_TEMPERATURE = "TEMP";
+    private static final String TOPIC_HUMIDITY = "HUMI";
+    private static final String TOPIC_GAS = "GAS";
+    private static final String TOPIC_LED = "LED";
+
+    TextView tvTemp,tvHumidity, tvGas, tvLedOn, tvLedOff;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,45 +32,102 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String serverUri = "tcp://35.222.45.221:1883";
-        String clientId = "client";
+        String clientId = "SMARTPHONE";
+
         mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        connectToMQTTBroker();
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(TAG, "Connection lost: " + cause.getMessage());
+            }
 
-        String username = "thanhduy";
-        String password = "thanhduy";
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String payload = new String(message.getPayload());
+                Log.d("MQTT", "Received message on topic: " + topic + " , " + payload);
+                if (topic.equals(TOPIC_TEMPERATURE)) {
+                    // xử lý dữ liệu nhiệt độ
+                    tvTemp = findViewById(R.id.temperature_value_text_view);
+                    tvTemp.setText(payload);
+                    //
+                } else if (topic.equals(TOPIC_HUMIDITY)) {
+                    // xử lý dữ liệu độ ẩm
+                    tvHumidity = findViewById(R.id.temperature_value_text_view);
+                    tvHumidity.setText(payload);
+                } else if(topic.equals(TOPIC_GAS)){
+                    tvGas = findViewById(R.id.gas_value_text_view);
+                    tvGas.setText(payload);
+                }
+            }
 
-        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setUserName(username);
-        mqttConnectOptions.setPassword(password.toCharArray());
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
 
+            }
+        });
+    }
+
+    public void connectToMQTTBroker(){
         try {
+            String username = "thanhduy";
+            String password = "thanhduy";
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
             IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // Connection success
-                    Log.d("MQTT", "Success");
+                    Log.d("MQTT", "Connection Success");
+                    subscribeToTopic();
+
+                    publishMessage("duy", "Hello duy", 2);
+
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                     // Connection failed
-                    Log.d("MQTT", "Failed");
+                    Log.d("MQTT", "Connection Failed");
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
 
-//        try {
-//            mqttAndroidClient.publish(topic, new MqttMessage(payload.getBytes()));
-//        } catch (MqttException e) {
-//            e.printStackTrace();
-//        }
+    public void subscribeToTopic(){
+        String topic = "SENSORS";
+        int qos = 1;
+        try {
+            IMqttToken subToken = mqttAndroidClient.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                    Log.d("MQTT", "Subscribed to topic: " + topic + ", qos: " + qos);
+                }
 
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d("MQTT", "Failed to subscribe.");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
 
-
-
-
-
+    public void publishMessage(String topic, String payload, int qos){
+        try {
+            MqttMessage message = new MqttMessage(payload.getBytes());
+            message.setQos(qos);
+            mqttAndroidClient.publish(topic, message);
+            Log.d("MQTT", "Topic: " + topic + ", Message: " + message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
